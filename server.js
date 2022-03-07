3513fca168dd99a1b0aaf2e6d508097fb98662ff
EC2 Default User <ec2-user@ip-172-31-6-159.eu-west-2.compute.internal>
2022-03-07 06:47:12 +0000

    mar

--- server.js
+++ server.js
@@ -911,7 +911,7 @@ app.use((req, res, next) => {
         6. Script (No need to set type="text/js" as it is default. No need to set src="" as this is not an external library. No need to set async/defer as it requries src attribute also. No need to set async/defer as your requirement is to execute js after html is parsed and htmlload event absolutely meets your requirements)
         7. Noscript (execute if no javascript is enabled. Ignore this tag.)
         -->
-        <title>VISTEK Vehicle Information System2</title>
+        <title>VISTEK Vehicle Information System</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Vehicle Identification System - VISTEK.">
